af064dd1d82a5efaedfbbb019d8f3915ab62706a
Vjeko <vjeko.pavicin@gmail.com>
2025-03-14 14:52:10 +0100

    Add `is_featured` column to the product table
    Added a new `is_featured` column to the `product` table with a default value of `FALSE` and ensured it is non-nullable. Adjusted other table structures by dropping unnecessary columns like `address_id` from `store` and setting the `price` column in `product` as non-nullable. These changes aim to streamline the database schema and support upcoming features.

--- src/main/resources/db/migration/V1_0_1__Add_is_featured_to_product.sql
+++ src/main/resources/db/migration/V1_0_1__Add_is_featured_to_product.sql
@@ -1,36 +1,11 @@
-ALTER TABLE product_image
-    DROP CONSTRAINT fk6oo0cvcdtb6qmwsga468uuukk;
-
-ALTER TABLE users
-    DROP CONSTRAINT fkditu6lr4ek16tkxtdsne0gxib;
-
-ALTER TABLE store_media
-    DROP CONSTRAINT fkfb72isuwppif6jufg89mwu382;
-
-ALTER TABLE users
-    DROP CONSTRAINT fkhmd3m5tgfs282j7f6svl8kalp;
-
-ALTER TABLE store
-    DROP CONSTRAINT fkp2sen6ouwnlht537csk0kip90;
-
 ALTER TABLE product
     ADD is_featured BOOLEAN default FALSE;
 
 ALTER TABLE product
     ALTER COLUMN is_featured SET NOT NULL;
 
-DROP TABLE product_image CASCADE;
-
-DROP TABLE store_media CASCADE;
-
 ALTER TABLE store
     DROP COLUMN address_id;
-
-ALTER TABLE users
-    DROP COLUMN address_id;
-
-ALTER TABLE users
-    DROP COLUMN store_id;
 
 ALTER TABLE product
     ALTER COLUMN price SET NOT NULL;
