12c7271c302f5356861d2d0fc60a0761cca86a4a
Vjeko <vjeko.pavicin@gmail.com>
2025-03-22 10:47:04 +0100

    Set up initial database schema with migrations
    Add baseline migration script (V1_0_0) including tables, sequences, and constraints for core entities such as users, store, product, and address. Introduce migration (V1_0_1) to add a unique store address column and foreign key.

---
+++ src/main/resources/db/migration/V1_0_1__Add_store_address.sql
@@ -0,0 +1,8 @@
+ALTER TABLE store
+    ADD address_id BIGINT;
+
+ALTER TABLE store
+    ADD CONSTRAINT uc_store_address UNIQUE (address_id);
+
+ALTER TABLE store
+    ADD CONSTRAINT FK_STORE_ON_ADDRESS FOREIGN KEY (address_id) REFERENCES address (id);
